4505111702f68696c5d42cd0b24a09ba1d0607d1
john <johnbuevich@yahoo.com>
2018-10-17 22:47:22 +0300

    added list food in basket

--- src/components/AdminPanelPage/RightPanel/BodyRight/WindowApp/BasketWindow/ProductContainer/FoodContainer/index.js
+++ src/components/AdminPanelPage/RightPanel/BodyRight/WindowApp/BasketWindow/ProductContainer/FoodContainer/index.js
@@ -4,11 +4,45 @@ import './style.css';
 
 
 export default (props) => {
+    let listFood = [];
+
+    const urlImg = 'https://diabet-expert.com/wp-content/uploads/2017/12/regulyarnoe-upotreblenie-fruktov-v-pischu-ukreplya-480x320.jpg';
+
+    for (let i = 0; i < 5; i++) {
+        listFood.push(<div className="components-RightPanel-BodyRight-WindowApp-BasketWindow-ProductContainer-FoodContainer-list">
+            <div className="components-RightPanel-BodyRight-WindowApp-BasketWindow-ProductContainer-FoodContainer-item">
+                <div className="components-RightPanel-BodyRight-WindowApp-BasketWindow-ProductContainer-FoodContainer-imgContainer">
+                    <img
+                        className="components-RightPanel-BodyRight-WindowApp-BasketWindow-ProductContainer-FoodContainer-image"
+                        src={urlImg}
+                    />
+                </div>
+                <div className="components-RightPanel-BodyRight-WindowApp-BasketWindow-ProductContainer-FoodContainer-info">
+                    <div className="components-RightPanel-BodyRight-WindowApp-BasketWindow-ProductContainer-FoodContainer-consist">
+                        <div className="components-RightPanel-BodyRight-WindowApp-BasketWindow-ProductContainer-FoodContainer-title">
+                            Клаб-сэндвич 300 г
+                        </div>
+                        <div className="components-RightPanel-BodyRight-WindowApp-BasketWindow-ProductContainer-FoodContainer-cost">
+                            2010 Р
+                        </div>
+                    </div>
+                    <div className="components-RightPanel-BodyRight-WindowApp-BasketWindow-ProductContainer-FoodContainer-controls">
+                        <div className="components-RightPanel-BodyRight-WindowApp-BasketWindow-ProductContainer-FoodContainer-quantity">
+                            5
+                        </div>
+                        <button className="components-RightPanel-BodyRight-WindowApp-BasketWindow-ProductContainer-FoodContainer-minus components-RightPanel-BodyRight-WindowApp-BasketWindow-ProductContainer-FoodContainer-button" />
+                        <button className="components-RightPanel-BodyRight-WindowApp-BasketWindow-ProductContainer-FoodContainer-plus components-RightPanel-BodyRight-WindowApp-BasketWindow-ProductContainer-FoodContainer-button" />
+                        <button className="components-RightPanel-BodyRight-WindowApp-BasketWindow-ProductContainer-FoodContainer-delete components-RightPanel-BodyRight-WindowApp-BasketWindow-ProductContainer-FoodContainer-button" />
+                    </div>
+                </div>
+            </div>
+        </div>);
+    }
 
     return <div
         className="components-RightPanel-BodyRight-WindowApp-BasketWindow-ProductContainer-FoodContainer-root"
         style={props.config.style}
     >
-        заказанные штуки
+        {listFood}
     </div>
 };
--- src/components/App.js
+++ src/components/App.js
@@ -15,7 +15,7 @@ export default class App extends React.Component {
     constructor() {
         super();
         this.state = {
-            page: 'loginPage'
+            page: 'adminPanelPage'
         };
     }
     //
