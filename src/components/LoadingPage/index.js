ff83309724d985e0ab15984dc650604ce4d591e0
john <johnbuevich@yahoo.com>
2018-10-29 17:54:18 +0300

    loader

---
+++ src/components/LoadingPage/index.js
@@ -0,0 +1,20 @@
+import React from "react";
+
+import configPage from "../config/configPage";
+
+import './style.css';
+
+
+export default class RegisterPage extends React.Component {
+
+    constructor() {
+        super();
+    }
+
+    render () {
+        return <div className="LoadingPage-root">
+
+        </div>
+    }
+};
+
